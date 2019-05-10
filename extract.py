84ea347244812ce05f46daf5edc17269eb3ea1a0
何悦 <vipheyue@foxmail.com>
2019-05-10 09:56:23 +0800

    更新文档

--- extract.py
+++ extract.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def deal_line(src_file):
@@ -122,12 +123,12 @@ if __name__ == '__main__':
     其他: 遇到.ds 文件报错 用命令行删除.ds即可
     """
 
-    dimens_xml_file = r"/Users/heyue/Desktop/testPy/beeboxes/packages/apps/DataTool/app/src/main/res/values-1280x800/dimens.xml"  # 输出dimens.xml文件 之后需要手动粘贴
+    dimens_xml_file = r"/Users/heyue/Documents/fenghe/fixface/beeboxes/packages/apps/BboxSetupWizard/app/src/main/res/values-1280x800/dimens.xml"  # 输出dimens.xml文件 之后需要手动粘贴
 
-    inputDir = r"/Users/heyue/Desktop/testPy/beeboxes/packages/apps/DataTool/app/src/main/res/layout"  # layout  输入文件夹
+    inputDir = r"/Users/heyue/Documents/fenghe/fixface/beeboxes/packages/apps/BboxSetupWizard/app/src/main/res/layout"  # layout  输入文件夹
     outputDir = r"/Users/heyue/Desktop/img"  # layout 输出文件夹
     extra_layout(inputDir, outputDir)  # 抽取layout
 
-    input_style_file = r"/Users/heyue/Desktop/testPy/beeboxes/packages/apps/DataTool/app/src/main/res/values/styles.xml"  # style.xml文件
-    output_style_file = r"/Users/heyue/Desktop/testPy/beeboxes/packages/apps/DataTool/app/src/main/res/values-1280x800/styles.xml"  # style.xml文件
+    input_style_file = r"/Users/heyue/Documents/fenghe/fixface/beeboxes/packages/apps/BboxSetupWizard/app/src/main/res/values/styles.xml"  # style.xml文件
+    output_style_file = r"/Users/heyue/Desktop/img/styles.xml"  # 输出style.xml文件
     extra_style(input_style_file, output_style_file)  # 抽取style.xml
